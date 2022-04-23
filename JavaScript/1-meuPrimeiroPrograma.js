ddbef12f5d6da977d9fa2a9082e9ad967b54b972
Gustavo <gustavofelix01@yahoo.com>
2022-04-23 12:04:48 -0300

    Initial commit

---
+++ JavaScript/1-meuPrimeiroPrograma.js
@@ -0,0 +1,17 @@
+console.log("Meu Primeiro programa: trabalhando com variáveis");
+//JS e case sensitive
+
+const idade = 26;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+
+/* cd C:\Users\guste\Documents\GitHub\FrontEnd\JavaScript> 
+   node .\1-meuPrimeiroPrograma.js 
+*/
